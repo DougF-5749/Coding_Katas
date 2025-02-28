d9e1de94665066932c85cad59a5edc84dc1dda5f
Douglas Fairfield <douglasfairfield@mac.lan>
2025-02-28 12:32:19 +0000

    intercection_union_difference set operation passed

--- katas/set_operations.py
+++ katas/set_operations.py
@@ -2,8 +2,12 @@
 # Task: Given two sets, compute their intersection, union, and difference. Return the three sets in a dictionary.
 # Example: A = {1,2,3}, B = {2,3,4} -> {Intersection: {2,3}, Union: {1,2,3,4}, Difference: {1}}
 
-def intercection_union_difference():
-    pass
+def intercection_union_difference(set1, set2):
+    return {
+        'Intersection' : set1.intersection(set2),
+        'Union' : set1.union(set2),
+        'Difference' : set1.difference(set2)
+    }
 
 # Check Disjoint Sets
 # Task: Given two sets, determine if they have no elements in common.
