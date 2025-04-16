c100f3aab3c6d1a61515652c2bfe76cea447d41e
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-04-16 12:13:18 +0100

    imported all fucntions from data_transformations.py (none exist yet)

---
+++ tests/test_data_transformations.py
@@ -0,0 +1,7 @@
+from katas.data_transformations import *
+
+# Convert a JSON string like the one below to a Python dictionary and returns the product name and price
+# JSON string = '{"product": "Laptop", "price": 1200, "available": true}'
+
+def test_return_name_and_price():
+    pass
