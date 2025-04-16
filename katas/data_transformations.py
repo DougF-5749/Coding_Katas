3d1df76f350abd958af76b0933c5c83d0b1cd775
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-04-16 12:22:07 +0100

    json_string added as argumant for return_name_and_price() function in data_transformations.py

--- katas/data_transformations.py
+++ katas/data_transformations.py
@@ -1,5 +1,5 @@
 # Convert a JSON string like the one below to a Python dictionary and returns the product name and price
 # JSON string = '{"product": "Laptop", "price": 1200, "available": true}'
 
-def return_name_and_price():
+def return_name_and_price(json_string):
     pass
