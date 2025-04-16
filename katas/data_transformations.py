8a6d6320981d293a2c50451d0ea5c16d2d2a5b57
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-04-16 12:18:40 +0100

    function defined for kata 1

--- katas/data_transformations.py
+++ katas/data_transformations.py
@@ -1,3 +1,5 @@
 # Convert a JSON string like the one below to a Python dictionary and returns the product name and price
 # JSON string = '{"product": "Laptop", "price": 1200, "available": true}'
 
+def return_name_and_price():
+    pass
