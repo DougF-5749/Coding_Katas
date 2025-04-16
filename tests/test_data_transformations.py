06c43f746fd72e8a50047972e3998e6668bfa88d
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-04-16 12:19:11 +0100

    test written for kata 1 in data transformations

--- tests/test_data_transformations.py
+++ tests/test_data_transformations.py
@@ -4,4 +4,11 @@ from katas.data_transformations import *
 # JSON string = '{"product": "Laptop", "price": 1200, "available": true}'
 
 def test_return_name_and_price():
-    pass
+    json_string = '{"product": "Laptop", "price": 1200, "available": true}'
+    actual = return_name_and_price(json_string)
+    expected = {
+        "price": 1200,
+        "product": "Laptop"
+    }
+
+    assert actual == expected
