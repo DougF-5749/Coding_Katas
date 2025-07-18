e25fb2db11e48de3d23993a7f1f0d64d1b893147
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-07-18 10:34:38 +0100

    most_frequernt words pass tests

--- katas/dictionary_manipulaptions.py
+++ katas/dictionary_manipulaptions.py
@@ -1,3 +1,5 @@
+import string
+from collections import Counter
 # Count Word Frequencies
 # Task: Given a list of words, build a dictionary mapping each word to its count.
 # Example: ["apple", "banana", "apple"] -> {"apple": 2, "banana": 1}.
@@ -22,8 +24,19 @@ def count_word_frequencies(lst):
 # Task: Given a list of words, group them together if they are anagrams of each other.
 # Example: ["eat","tea","tan","ate","nat","bat"] -> [["eat","tea","ate"], ["tan","nat"], ["bat"]].
 
-def most_frequent_word():
-    pass
+def most_frequent_word(lst):
+    # split the string ainto list
+    split = lst.split()
+    # remove non-alpha characters from the start/end of each word in the list and make lowercase
+    cleaned_words = [word.strip(string.punctuation).lower() for word in split]
+    # import the counter from collections and count that list
+    counts = Counter(cleaned_words)
+    # determin the max count value in the Counter object
+    max_count = max(counts.values())
+    # gather all words from Counter object frequency == to max_count
+    candidates = [word for word, count in counts.items() if count == max_count]
+    # return the min value from candidates (returns alphabetically in case of multiple candidates)
+    return min(candidates)
 
 def group_anagrams():
     pass
