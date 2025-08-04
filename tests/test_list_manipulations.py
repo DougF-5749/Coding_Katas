eb15f99ed376a023c92bbc5669a0e2993434cdaf
Douglas Fairfield <fairfielddouglas@gmail.com>
2025-08-04 08:37:00 +0100

    runlength_decode tests created

--- tests/test_list_manipulations.py
+++ tests/test_list_manipulations.py
@@ -287,6 +287,13 @@ def test_decode_matrix():
     expedcted_2 = "abcd"
     assert actual_2 == expedcted_2
 
+def test_runlength_decode():
+    assert runlength_decode("a3b")  == "aaab"
+    assert runlength_decode("a\\3b2")      == "a3bb"       # `\\3` means literal "3"
+    assert runlength_decode("a\\\\2b")     == "a\\b"       # `\\\\` → literal `\` followed by 2 → "\\" x 2 = "\\"
+    assert runlength_decode("x\\2y\\5z")   == "x2y5z"
+    assert runlength_decode("a2\\1b")      == "aab1b"
+
 # Merge Sort
 # Task: Merge two sorted arrays together to create a new sorted array
 # Example: [1, 3, 5] and [2, 4, 6] -> [1, 2, 3, 4, 5, 6]
